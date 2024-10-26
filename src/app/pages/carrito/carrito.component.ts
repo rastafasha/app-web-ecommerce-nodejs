7f6344c121d658c4e7163d5dc5a8d74f0e662f69
rastafasha <mercadocreativo@gmail.com>
2024-10-25 21:44:53 -0400

    conectado trasnferencia y venta

--- src/app/pages/carrito/carrito.component.ts
+++ src/app/pages/carrito/carrito.component.ts
@@ -232,6 +232,7 @@ export class CarritoComponent implements OnInit {
       // llamo al servicio
       this._trasferencias.createTransfer(this.formTransferencia.value).subscribe(resultado => {
         console.log('resultado: ',resultado);
+        this.verify_dataComplete();
         if(resultado.ok){
           // transferencia registrada con exito
           console.log(resultado.payment);
@@ -638,7 +639,7 @@ export class CarritoComponent implements OnInit {
         codigo_cupon : this.cupon,
         info_cupon :  this.info_cupon_string,
         idtransaccion : null,
-        metodo_pago : 'Paypal',
+        // metodo_pago : 'Paypal',
 
         tipo_envio: this.medio_postal.tipo_envio,
         precio_envio: this.medio_postal.precio,
@@ -662,7 +663,97 @@ export class CarritoComponent implements OnInit {
 
   }
 
-  back_data(){
+  verify_dataComplete(){debugger
+    if(this.id_direccion){
+      this.msm_error = '';
+      $('#btn-verify-data').animate().hide();
+      $('#btn-back-data').animate().show();
+
+      $('#card-data-envio').animate().show();
+
+      $('#card-pay').animate().show('fast');
+      $('.cart-data-venta').animate().hide('fast');
+
+
+
+      if(this.data_cupon){
+        if(this.data_cupon.categoria){
+          this.info_cupon_string = this.data_cupon.descuento + '% de descuento en ' + this.data_cupon.categoria.nombre;
+        }else if(this.data_cupon.subcategoria){
+          this.info_cupon_string = this.data_cupon.descuento + '% de descuento en ' + this.data_cupon.subcategoria;
+        }
+      }
+
+      var fecha = new Date();
+
+      var months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Novimbre", "Deciembre"];
+      fecha.setDate(fecha.getDate() + parseInt(this.medio_postal.dias));
+      this.date_string =  fecha.getDate() +' de ' + months[fecha.getMonth()] + ' del ' + fecha.getFullYear();
+
+
+      this.data_venta = {
+        user : this.identity.uid,
+        total_pagado : this.subtotal,
+        codigo_cupon : this.cupon,
+        info_cupon :  this.info_cupon_string,
+        idtransaccion : null,
+        metodo_pago : this.selectedMethod,
+        // metodo_pago : 'Paypal',
+
+        tipo_envio: this.medio_postal.tipo_envio,
+        precio_envio: this.medio_postal.precio,
+        tiempo_estimado: this.date_string,
+
+        direccion: this.data_direccion.direccion,
+        destinatario: this.data_direccion.nombres_completos,
+        detalles:this.data_detalle,
+        referencia: this.data_direccion.referencia,
+        pais: this.data_direccion.pais,
+        ciudad: this.data_direccion.ciudad,
+        zip: this.data_direccion.zip,
+      }
+
+      console.log(this.data_venta);
+
+      this.saveVenta();
+
+    }else{
+      this.msm_error = "Seleccione una dirección de envio.";
+    }
+
+  }
+
+  saveVenta(){debugger
+    this._ventaService.registro(this.data_venta).subscribe(response =>{
+      this.data_venta.detalles.forEach(element => {
+        console.log(element);
+        this._productoService.aumentar_ventas(element.producto._id).subscribe(
+          response =>{
+          },
+          error=>{
+            console.log(error);
+
+          }
+        );
+          this._productoService.reducir_stock(element.producto._id,element.cantidad).subscribe(
+            response =>{
+              this.remove_carrito();
+              this.listar_carrito();
+              this.socket.emit('save-carrito', {new:true});
+              this.socket.emit('save-stock', {new:true});
+              this._router.navigate(['/app/cuenta/ordenes']);
+            },
+            error=>{
+              console.log(error);
+
+            }
+          );
+      });
+
+    },)
+    }
+
+    back_data(){
     $('#btn-verify-data').animate().show();
     $('#btn-back-data').animate().hide();
 
