8c56cd95f4006984971b8250ec2c3b5820bd6645
rastafasha <mercadocreativo@gmail.com>
2024-10-28 15:41:35 -0400

    preview con pagos

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,14 @@
 
 export const environment = {
   production: true,
-  baseUrl: 'http://localhost:3003/api',
-  mediaUrl: 'http://localhost:3003/api/uploads/',
-  soketServer : 'http://localhost:3003',
+  // baseUrl: 'http://localhost:3003/api',
+  // mediaUrl: 'http://localhost:3003/api/uploads/',
+  // soketServer : 'http://localhost:3003',
+  //remoto vercel
+  baseUrl: 'https://back-ecomm-mall.vercel.app/api',
+  mediaUrl: 'https://back-ecomm-mall.vercel.app/api/uploads/',
+  soketServer : 'https://back-ecomm-mall.vercel.app/',
+  //pluggins
   rapidapiKey: 'a7036a3222mshc2920e679cd1cafp141e56jsn81cbe707ac15',
   rapidapiHost: 'apidojo-17track-v1.p.rapidapi.com',
   clientIdPaypal: 'AXlazeNsZ0CmjfJIronSzcqzw4hLHkcoVEM5fO5BY7AbD-_GhKoKezRcavq6-T4kQuRqaTXFB_VXmheG',
